1347602d307f60ac8de5cc95486c63612a4f7a85
halinad <gaxin@list.ru>
2023-02-28 01:18:14 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function toReadable (number) {
-  
-}
+    if (number === 0) {return 'zero'}
+
+    const o = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const td = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const doz = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const h = ['hundred']
+
+    
+    if (number <= 9){
+        return o[number]}
+    if (number >= 10 && number <= 19){
+        return td[number - 10]}
+
+    if (number >= 20 && number <= 99 && number%10 === 0)
+    {return doz[Math.floor(number / 10)]}
+
+   if (number >= 20 && number <= 99)
+   {return doz[Math.floor(number / 10)] + " " + o[number % 10]}
+
+    
+
+    if (number >= 100 && number%100 === 0){
+        return o[Math.floor(number / 100)] + " " + h}
+
+    if (number >= 100 && number%10 === 0){
+        return o[Math.floor(number / 100)] + " " + h + " " + doz[Math.floor((number % 100) / 10)]}
+
+    if (number >= 100 && number%100 <= 9){
+        return o[Math.floor(number / 100)] + " " + h + " " + o[number % 10]}
+
+    if (number >= 100 && number%100 >= 9 && number%100 < 20){
+        return o[Math.floor(number / 100)] + " " + h + " " + td[number % 10]}
+
+    else {return o[Math.floor(number / 100)] + " " + h + " " + doz[Math.floor((number % 100) / 10)] + " " + o[number % 10]}
+     }
+     
+     //console.log(toReadable(983));
+
